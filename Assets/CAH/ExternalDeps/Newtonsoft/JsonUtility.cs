c1fc5b983f6ca04d966c748b909d525689ffd73e
shlifedev <shlifedev@gmail.com>
2021-07-27 07:38:59 +0900

    asmdef

---
+++ Assets/CAH/ExternalDeps/Newtonsoft/JsonUtility.cs
@@ -0,0 +1,13 @@
+﻿ 
+public static class JsonUtility
+{
+    public static string ToJson(this object target)
+    { 
+        return Newtonsoft.Json.JsonConvert.SerializeObject(target);
+    }
+
+    public static T JsonToObject<T>(this string jsonString)
+    {
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonString);
+    }
+}
